ea1c3faa02ac329b452c16af60b78955a02e4467
Alex <23Alexx24@gmail.com>
2015-07-30 15:48:24 +0300

    Удалил несуществующий юзинг

--- Domain.Entities/Entities/User.cs
+++ Domain.Entities/Entities/User.cs
@@ -1,11 +1,7 @@
-﻿using Domain.Entities.Constants;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Entities
 {

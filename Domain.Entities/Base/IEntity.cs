8e9241ad79dd50e90564b86db1c094c61a7de4ad
Alex <23Alexx24@gmail.com>
2015-07-30 14:21:13 +0300

    Добавил IRepository.cs и IUnitOfWork.cs

--- Domain.Entities/Base/IEntity.cs
+++ Domain.Entities/Base/IEntity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    interface IEntity
+    public interface IEntity
     {
     }
 }
---
+++ Domain.Services/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace Domain.Services.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        /// <summary>Возвращает экземпляр сервиса</summary>
+        /// <typeparam name="TEntity">Тип сущности, с которой работает репозиторий</typeparam>
+        IRepository<TEntity> GetRepository<TEntity>() where TEntity : class, IEntity;
+
+        void SaveChanges();
+
+        Task SaveChangesAsync();
+
+        void BeginTransaction();
+
+        void RollBackTransaction();
+
+        void CommitTransaction();
+    }
+}

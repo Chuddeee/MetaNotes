6138f0190344d5fdb9573262312bae2360acf594
Alex <23Alexx24@gmail.com>
2015-08-05 12:28:51 +0300

    Добавил нугет пакеты для джиквери и бутстрапа. Настройка бандлов

---
+++ MetaNotes/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+namespace MetaNotes
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/scripts/main").Include(
+                        "~/Scripts/jquery/jquery*",
+                        "~/Scripts/bootstrap/bootstrap*"));
+
+            bundles.Add(new StyleBundle("~/bundles/styles/main").Include(
+                "~/Content/Styles/bootstrap/bootstrap*"));
+        }
+    }
+}
